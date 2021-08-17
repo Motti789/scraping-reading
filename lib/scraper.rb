0c114e0f40d554bd9ee8bf41cb0a062b3c5aca52
Motti789 <mchillywilly@gmail.com>
2021-08-17 14:12:01 -0400

    commit tree 7d598d1935c9e6caef972a484a255b45d373e3aa

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,15 @@ require 'nokogiri'
 require 'open-uri'
 
 html = URI.open("https://flatironschool.com/")
+
+doc = Nokogiri.HTML5(html)
+# courses = 
+p doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")[0].attributes
+
+# courses.each do |course|
+#     puts course.text.strip
+# end
+
+
+
+
